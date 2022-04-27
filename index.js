68adaff353b9f8b5e737c74b3b43bc66636cf953
Rasel-ahemd <nhd305812@gmail.com>
2022-04-27 20:20:57 +0500

    second commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+// midleware
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("all data");
+});
+
+// pass=hSMFLPo6zrAqzr8w
+// name=
+
+const uri =
+  "mongodb+srv://mongodbUser:hSMFLPo6zrAqzr8w@cluster0.6plls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const fbPostDatabase = client.db("fbPosts").collection("posts");
+    const name = { name: "rasel", age: 21 };
+    // const result = await fbPostDatabase.insertOne(name);
+    // get all posts
+    app.get("/posts", async (req, res) => {
+      const query = {};
+      const cursor = fbPostDatabase.find(query);
+      const posts = await cursor.toArray();
+      res.send(posts);
+    });
+    // Insert fbPosts
+    app.post("/posts", async (req, res) => {
+      const newPost = req.body;
+      const result = await fbPostDatabase.insertOne(newPost);
+      res.send(result);
+    });
+  } finally {
+    //  await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port);
